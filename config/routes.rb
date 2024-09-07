6fcce5aaedb828f23646349f5d0ef625fc88f30f
Hayden Rouille <hayden@rouille.dev>
2024-09-07 13:58:43 +0100

    Add users / devise
    - Add a nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :trips do
     resources :activities, except: %i[index]
   end
@@ -9,5 +10,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root to: "home#index"
 end
